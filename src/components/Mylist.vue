b8ab8aa5d9676420d0508167a488da1d0face3a5
NiccoCorsini <nik.piky@gmail.com>
2021-05-14 14:01:27 +0200

    code cleanup

--- src/components/Mylist.vue
+++ src/components/Mylist.vue
@@ -72,35 +72,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.container {
-  display: flex;
-  flex-wrap: wrap;
-  max-width: 100%;
-  .noitems {
-    width: 100%;
-    height: calc(100vh - 200px);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .list {
-    display: flex;
-  }
-  .film {
-    width: 342px;
-    cursor: pointer;
-    padding: 2rem 1rem;
-    transition: transform 400ms;
-    position: relative;
-    &:hover {
-      transform: scale(1.1);
-    }
-    button {
-      position: absolute;
-      bottom: 36px;
-      right: 16px;
-      z-index: 6;
-    }
-  }
-}
+@import "../style/mylist";
 </style>
